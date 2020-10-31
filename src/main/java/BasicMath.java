60c031af12a3f4ccb14d68d22ac5dd4a812840ff
jmcginty865 <jmcgnty23@gmail.com>
2020-10-30 21:38:25 -0400

    Set up unit testing and wrote my first unit test for addition method.

--- src/main/java/BasicMath.java
+++ src/main/java/BasicMath.java
@@ -1,2 +1,6 @@
 public class BasicMath {
+
+    public int addition(int one, int two){
+        return 3;
+    }
 }
--- src/test/java/BasicMathTest.java
+++ src/test/java/BasicMathTest.java
@@ -1,2 +1,27 @@
+import org.junit.Assert;
+import org.junit.Before;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class BasicMathTest {
+
+    BasicMath BTM;
+
+    @Before
+    public void setup(){
+        BasicMath BTM = new BasicMath();
+    }
+
+    @Test
+    public void testAddition() {
+        // Run the test
+        final int result = BTM.addition(0, 0);
+
+        // Verify the results
+        Assert.assertEquals(0, result);
+    }
+
+
+
 }
